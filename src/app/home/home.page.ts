f65b1bc7f3553f7d7e55c4aafff39f1c939357f3
Senio Caires <senio.caires@pdcase.com.br>
2023-03-27 22:22:49 -0300

    App

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map: any;
+
   constructor() {}
 
+  async ionViewDidEnter() {
+
+    Geolocation.watchPosition({ enableHighAccuracy: true, maximumAge : 0 }, (coordinates) => {
+      console.log('Current position:', coordinates);
+      if (coordinates) {
+        this.map = L.map('mapId').setView([ coordinates.coords.latitude, coordinates.coords.longitude ], 20);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',  {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(this.map);
+
+        const markPoint = L.marker([ coordinates.coords.latitude, coordinates.coords.longitude ], {
+          title: 'Estou aqui',
+          icon: L.icon({
+            iconUrl: 'https://cdn.kernvalley.us/img/adwaita-icons/actions/mark-location.svg',
+            iconSize: [32, 32]
+        })});
+        markPoint.bindPopup('<p>Estou aqui!</p>')
+        this.map.addLayer(markPoint);
+      }
+    });
+
+  }
+
+  ionViewDidLeave(){
+    this.map.remove();
+  }
 }
